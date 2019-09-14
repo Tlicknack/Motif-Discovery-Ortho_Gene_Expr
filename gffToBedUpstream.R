2622958e18910693313975d5f3fb2fd30c299737
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2019-09-13 17:22:26 -0700

    this should work to now return a list of BED files named after each spp

--- gffToBedUpstream.R
+++ gffToBedUpstream.R
@@ -7,9 +7,15 @@ gffToBedUpstream = function(speciesNames, speciesGFFs){
   #speciesNames = c("Pdec", "Pnov")
   #speciesGFFs = c("~/Paramecium_GFF/pdec-gene.tab", "~/Paramecium_GFF/pnov-gene.tab")
   
+  lBEDs = list()
+  
   for(i in 1:length(speciesNames)){
     currentSp = speciesNames[i]
     currentGFF = read.table(speciesGFFs[i], as.is = T)
     allGeneCoords = apply(currentGFF, 1, upstreamCoordPlusMinus)  # This returns a list(scaf, start, stop, geneID, strand)
+    outName = paste(currentSp, "_Upstream.bed", sep="")
+    outBED = data.frame(matrix(unlist(allGeneCoords), nrow=nrow(currentGFF), byrow=T))  # This converts the list into a BED-like Data Frame
+    lBEDs[[currentSp]] = outBED
   }
+  return(lBEDs)
 }

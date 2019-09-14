a69e5277ffbbeb7c61b3ae524ba4b2a99fa19ff3
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2019-09-13 17:06:30 -0700

    should be done. returns list of scaf, start, stop, geneID, strand

--- upstreamCoordPlusMinus.R
+++ upstreamCoordPlusMinus.R
@@ -1,7 +1,17 @@
 upstreamCoordPlusMinus = function(rowVector){
-  scaf = rowVector[1]
-  firstCoord = rowVector[2]
-  secondCoord = rowVector[3]
-  strand = rowVector[4]
+  scaf = as.character(unlist(rowVector[1]))
+  firstCoord = as.numeric(unlist(rowVector[2]))
+  secondCoord = as.numeric(unlist(rowVector[3]))
+  strand = as.character(unlist(rowVector[4]))
+  geneID = as.character(unlist(rowVector[5]))
   
+  if(strand == "+"){
+    start = as.numeric(firstCoord)-200
+    stop = as.numeric(firstCoord)
+  }
+  if(strand == "-"){
+    start = as.numeric(secondCoord)
+    stop = as.numeric(secondCoord)+200
+  }
+  return(list(scaf, start, stop))
 }
